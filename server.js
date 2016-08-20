9628cbea2f830245375610a4ff6412040e8dc17e
Dhananjay <patel.iec@gmail.com>
2016-08-20 17:05:41 +0530

    first get api

--- server.js
+++ server.js
@@ -34,6 +34,19 @@ app.get("/todo", function(req, res){
     res.json(todos);
 })
 
+app.get("/todo/:id", function(req, res){
+    var todoId = req.params.id;
+    for(var i=0 ; i< todos.length; i++)
+        {
+            if(todos[i].id == todoId)
+                {
+                   res.json(todos[i]); 
+                    break;
+                }
+        }
+    
+})
+
 app.listen(PORT, function(){
     console.log("Server has started at port:"+PORT);
 })
