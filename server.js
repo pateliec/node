9706f859d9f07c250ac6aae85db9852f3f12f3c3
Dhananjay <patel.iec@gmail.com>
2016-08-20 16:44:35 +0530

    first get api

--- server.js
+++ server.js
@@ -4,8 +4,34 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id:1,
+    description: "Finish Node Api videos",
+    completed:false
+},
+             {
+    id:2,
+    description: "Go to Market",
+    completed:false
+},
+             {
+    id:3,
+    description: "Go to Temple",
+    completed:false
+},
+             {
+    id:4,
+    description: "Have lunch",
+    completed:true
+}
+]
+
 app.get("/", function(req, res){
-    res.send("Todo Api Root");
+    res.json("Todo Api Root");
+})
+
+app.get("/todo", function(req, res){
+    res.send(todos);
 })
 
 app.listen(PORT, function(){
